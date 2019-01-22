4384ea5f94e5bd0d9605b08bfaab8713822f998b
Django <admin@Admins-MacBook-Pro.local>
2019-01-22 15:36:30 -0500

    Adding refs

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,12 +5,26 @@ import classes from './Person.css';
 import withClass from '../../hoc/withClass';
 
 class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.textInput = React.createRef();
+    };
+
+    focusInput() {
+        this.textInput.current.focus();
+    }
+
+
+
     render() {
         return (
             <>
                 <p onClick={this.props.click}>My name is {this.props.name}, I am {this.props.age} years old</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name}></input>
+                <input type="text"
+                    onChange={this.props.changed}
+                    value={this.props.name}
+                    ref = {this.textInput}></input>
             </>
         )
     }
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -3,12 +3,21 @@ import Person from './Person/Person';
 
 //Map through the state.persons in App.js and create Person components.
 class Persons extends Component {
+    constructor(props) {
+        super(props);
+        this.lastPerson = React.createRef();
+    }
+    componentDidMount() {
+        this.lastPerson.current.focusInput();
+    }
+
     render() {
         return this.props.persons.map((person,index) => {
                 return <Person 
                 name={person.name}
                 age={person.age} 
                 click={this.props.clicked.bind(this,index)}
+                ref={this.lastPerson}
                 key={person.id}
                 changed={(event) => this.props.changed(event,person.id)} />
             })
